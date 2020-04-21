500e6176d685cd923c50c460fd497df2f35e8ae5
ali bougarne <bougarne.ali@gmail.com>
2020-04-21 12:08:09 +0100

    update dependencies

--- src/pages/tags/list/index.ts
+++ src/pages/tags/list/index.ts
@@ -3,6 +3,7 @@ import Component from 'vue-class-component';
 import { tagModule } from '@/store/modules/tag/tag.module';
 import Tag from '@/store/modules/tag/tag.entity';
 import TagComponent from '@/components/tag/TagComponent.vue';
+import gsap from 'gsap';
 @Component({
   components: { TagComponent }
 })
@@ -11,21 +12,35 @@ export default class TagsPage extends Vue {
 
   public async mounted(): Promise<void> {
     this.$q.loading.show({
-      delay: 400 // ms
+      // delay: 400 // ms
     });
     // this.tags = await tagModule.loadTags();
-    tagModule.loadTags().then((response: Tag[]) => {
-      this.tags = response;
-      console.log('%c⧭ tags===>', 'color: #f2ceb6', response);
-      setTimeout(() => {
-        this.$q.loading.hide();
-      }, 1000)
-      this.$nextTick(function () {
-        console.log(this.$el) // => 'updated'
+    tagModule
+      .loadTags()
+      .then((response: Tag[]) => {
+        this.tags = response;
+        setTimeout(() => {
+          this.$q.loading.hide();
+        }, 1000);
+        this.$nextTick(function() {
+          gsap.from(this.$el.childNodes, {
+            duration: 2,
+            scale: 0.5,
+            opacity: 0,
+            delay: 0.5,
+            stagger: 0.2,
+            ease: 'elastic',
+            force3D: false
+          });
+
+          // this.$el.childNodes.forEach((element:ChildNode,index:number) => {
+          //   console.log('%c⧭', 'color: #f200e2', element);
+          // });
+        });
       })
-    }).catch((error: Error) => {
-      this.$q.loading.hide();
-      console.log(error);
-    });
+      .catch((error: Error) => {
+        this.$q.loading.hide();
+        console.log(error);
+      });
   }
 }
