8efee9d6852a3fc0a4749d8398cb41953f81c9cd
ali bougarne <bougarne.ali@gmail.com>
2020-04-21 19:32:55 +0100

    adjust style for project listing, remove tooltip

--- src/components/tag/index.ts
+++ src/components/tag/index.ts
@@ -9,11 +9,14 @@ import Tag from '@/store/modules/tag/tag.entity';
 export default class TagComponent extends Vue { 
   @Prop() 
   public tag?:Tag;
+  @Prop() 
+  public noTooltip?:boolean; 
   
   get link_API():string{
     return process.env.API || "";
   }
   
   public mounted():void{
+    console.log('%c⧭ this.noTooltip', 'color: #aa00ff', this.noTooltip);
   }
 }
